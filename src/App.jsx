bcacae2611b9c2c9a39aba7990ec7acbcbef8956
RodrigoA15 <rapapamija@misena.edu.co>
2025-04-25 08:13:02 -0500

    Add: Map 24-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Map2024 } from "./components/Map2024";
+// import { Map2024 } from "./components/Map2024";
 // import MapViewer from "./components/MapViewer";
+import { Map2025 } from "./components/Map2025";
 
 function App() {
   return (
     <div>
-      <Map2024 />
+      <Map2025 />
     </div>
   );
 }
