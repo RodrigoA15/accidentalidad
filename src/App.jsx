fe5bc9305ed23f0fcc9569d9118483d8b1a4f222
RodrigoA15 <rapapamija@misena.edu.co>
2025-06-17 17:25:04 -0500

    Change: App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import { Map2024 } from "./components/Map2024";
 // import MapViewer from "./components/MapViewer";
 // import { Map2025 } from "./components/Map2025";
-import { Map21_25 } from "./components/Map21_25";
+import { Map2025 } from "./components/Map2025";
+// import { Map21_25 } from "./components/Map21_25";
 
 function App() {
   return (
     <div>
-      <Map21_25 />
+      <Map2025/>
     </div>
   );
 }
