d213ff7c8e440365cc749c55930e61b5ea04388e
RodrigoA15 <rapapamija@misena.edu.co>
2026-03-13 11:11:38 -0500

    Add: import maps App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 // import { Map2024 } from "./components/Map2024";
 // import MapViewer from "./components/MapViewer";
 // import { Map2025 } from "./components/Map2025";
-import { Map21_25 } from "./components/Map21_25";
+// import { Map21_25 } from "./components/Map21_25";
+// import { Comparendos } from "./components/comparendos";
+import { MapBoxExample } from "./components/MapBoxExample";
+// import { MapAll } from "./components/MapAll";
 
 function App() {
   return (
     <div>
-      <Map21_25/>
+      <MapBoxExample />
     </div>
   );
 }
 
 export default App;
+
+
+//!!NOTA: Cambiar de componente "principal" segun la necesidad
